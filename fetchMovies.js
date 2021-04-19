1e2a5cc92bda79e6f5b9e9155b351dcf4f764428
Maja Jensen <eaamnje@students.eaaa.dk>
2021-04-19 14:01:02 +0200

    More movies and style

--- fetchMovies.js
+++ fetchMovies.js
@@ -7,6 +7,30 @@ let movies = [
     {
         "fetchUrl": "https://www.omdbapi.com/?t=Space+Sweepers",
         "youtubeId": "https://www.youtube.com/watch?v=H1WYnJF1Pwo"
+    },
+    {
+        "fetchUrl": "http://www.omdbapi.com/?t=Godzilla%3A+King+of+the+Monsters",
+        "youtubeId": "https://www.youtube.com/watch?v=wVDtmouV9kM"
+    },
+    {
+        "fetchUrl": "http://www.omdbapi.com/?t=Arrival",
+        "youtubeId": "https://www.youtube.com/watch?v=tFMo3UJ4B4g"
+    },
+    {
+        "fetchUrl": "http://www.omdbapi.com/?t=the+martian",
+        "youtubeId": "https://www.youtube.com/watch?v=Ue4PCI0NamI"
+    },
+    {
+        "fetchUrl": "http://www.omdbapi.com/?t=Interstellar",
+        "youtubeId": "https://www.youtube.com/watch?v=zSWdZVtXT7E"
+    },
+    {
+        "fetchUrl": "http://www.omdbapi.com/?t=PASSENGERS",
+        "youtubeId": "https://www.youtube.com/watch?v=7BWWWQzTpNU"
+    },
+    {
+        "fetchUrl": "http://www.omdbapi.com/?t=The+Maze+Runner",
+        "youtubeId": "https://www.youtube.com/watch?v=AwwbhhjQ9Xk"
     }
 ];
 
